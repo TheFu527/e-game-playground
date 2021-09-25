2446f0e8653194b35b2a6228e676b179cd302074
cocoatouch <li.jinh@husky.neu.edu>
2021-09-26 01:33:28 +0800

    xml added

--- NUMAD21FaJinhongLi/app/src/main/java/edu/neu/madcourse/numad21fa_jinhongli/MainActivity.java
+++ NUMAD21FaJinhongLi/app/src/main/java/edu/neu/madcourse/numad21fa_jinhongli/MainActivity.java
@@ -1,7 +1,9 @@
 package edu.neu.madcourse.numad21fa_jinhongli;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,22 +11,40 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    // 先对控件进行声明
     private Button btm_about;
-// 先进行声明
+    private Button btm_clicky;
     private TextView textView;
+    private TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //启动程序
         super.onCreate(savedInstanceState);
+        //初始化界面,使用activity_main.xml 布局
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
-        btm_about = findViewById(R.id.btm_about);
+//        textView.setText("Hello");
+//        text1 = new TextView(this);
+//        text1.setText("world");
+//        text1.setId(R.id.text1 );
+//        ConstraintLayout myCL = new ConstraintLayout(this);
+//        setContentView(myCL);
+        btm_about = findViewById(R.id.btm_about1);
         //按钮按下触发操作
         btm_about.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Jinhong Li, li.jinh@northeastern.edu", Toast.LENGTH_LONG).show();
+            }
+        });
+        btm_clicky = findViewById(R.id.btm_clicky);
+        //按钮按下触发操作
+        btm_about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent clicker = new Intent(MainActivity.this,clickActivity.class);
+                startActivity(clicker);
             }
         });
     }
--- NUMAD21FaJinhongLi/app/src/main/java/edu/neu/madcourse/numad21fa_jinhongli/clickActivity.java
+++ NUMAD21FaJinhongLi/app/src/main/java/edu/neu/madcourse/numad21fa_jinhongli/clickActivity.java
@@ -3,9 +3,19 @@ package edu.neu.madcourse.numad21fa_jinhongli;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class clickActivity extends AppCompatActivity {
 
+    private Button A;
+    private Button B;
+    private Button C;
+    private Button D;
+    private Button E;
+    private Button F;
+    private TextView textView;
+    private TextView text1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
