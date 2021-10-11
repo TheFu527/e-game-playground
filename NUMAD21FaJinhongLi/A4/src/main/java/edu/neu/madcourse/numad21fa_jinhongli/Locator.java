2411fc1769feb1f09693439a4df89fd697050355
cocoatouch <li.jinh@husky.neu.edu>
2021-10-11 00:55:35 -0700

    A5 Final

--- NUMAD21FaJinhongLi/A4/src/main/java/edu/neu/madcourse/numad21fa_jinhongli/Locator.java
+++ NUMAD21FaJinhongLi/A4/src/main/java/edu/neu/madcourse/numad21fa_jinhongli/Locator.java
@@ -1,49 +1,63 @@
 package edu.neu.madcourse.numad21fa_jinhongli;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Locator extends AppCompatActivity {
     TextView Vlatitude;
     TextView Vlongtitude;
-
+    private int LOCATION_PERMISSION_CODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locator);
         Vlatitude = findViewById(R.id.latitude);
         Vlongtitude = findViewById(R.id.longitude);
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        String locationProvider = LocationManager.NETWORK_PROVIDER;
         //开始定位,获取包含上次记录的位置数据的位置对象
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
-                (this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+                (this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission requierd",Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
+        } else {
+            Toast.makeText(this, "Already granted this permission",Toast.LENGTH_SHORT).show();
+            LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            Vlatitude.setText("Latitude:" + latitude);
+            Vlongtitude.setText("Longtitude:" + longitude);
         }
-        Location location = locationManager.getLastKnownLocation(locationProvider);
-        //获取纬度
-        Double latitude = location.getLatitude();
-        //获取经度
-        Double longitude = location.getLongitude();
-        Vlatitude.setText("Latitude" + latitude);
-        Vlongtitude.setText("Longtitude:" + longitude);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_CODE)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+                @SuppressLint("MissingPermission") Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+                Vlatitude.setText("Latitude:" + latitude);
+                Vlongtitude.setText("Longtitude:" + longitude);
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 }
