4802eaf08bf1696f7495722ed8639c73a67c9d41
zhugelijuan123 <nancyrain2015@163.com>
2021-12-08 17:11:57 +0800

    userinfodto add token

--- EgamePlaygound/app/src/main/java/edu/neu/madcourse/numad21fa/egameplaygound/model/dto/UserInfoDTO.java
+++ EgamePlaygound/app/src/main/java/edu/neu/madcourse/numad21fa/egameplaygound/model/dto/UserInfoDTO.java
@@ -1,7 +1,14 @@
 package edu.neu.madcourse.numad21fa.egameplaygound.model.dto;
 
+import com.google.firebase.database.Exclude;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import edu.neu.madcourse.numad21fa.egameplaygound.constant.enums.user.UserGenderEnum;
 import edu.neu.madcourse.numad21fa.egameplaygound.constant.enums.user.UserLevelEnum;
+import edu.neu.madcourse.numad21fa.egameplaygound.ui.me.UserInfo;
 
 public class UserInfoDTO {
     private String uuid;
@@ -11,6 +18,32 @@ public class UserInfoDTO {
     private UserLevelEnum level;
     private String email;
     private String location;
+    private String tokenid;
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("uid", uuid);
+        result.put("name", name);
+        result.put("avatarURI", avatarURI);
+        result.put("gender", gender);
+        result.put("level", level);
+        result.put("email", email);
+        result.put("location", location);
+        result.put("tokenid", tokenid);
+        return result;
+    }
+    public UserInfoDTO(String uuid,String name,String avatarURI, UserGenderEnum gender,UserLevelEnum level,String email,String location,String tokenid) {
+        this.uuid = uuid;
+        this.name = name;
+        this.avatarURI = avatarURI;
+        this.gender = gender;
+        this.level = level;
+        this.email = email;
+        this.location = location;
+        this.tokenid = tokenid;
+    }
+
 
     public UserInfoDTO() {
         this.uuid = "";
@@ -20,7 +53,34 @@ public class UserInfoDTO {
         this.level = UserLevelEnum.UNKNOWN;
         this.email = "";
         this.location = "";
+        this.tokenid = "";
     }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserInfoDTO that = (UserInfoDTO) o;
+        return Objects.equals(uuid, that.uuid) && Objects.equals(name, that.name) && Objects.equals(avatarURI, that.avatarURI) && gender == that.gender && level == that.level && Objects.equals(email, that.email) && Objects.equals(location, that.location) && Objects.equals(tokenid, that.tokenid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid, name, avatarURI, gender, level, email, location, tokenid);
+    }
+
+
+    public UserInfoDTO setTokenid(String tokenid) {
+        this.tokenid = tokenid;
+        return this;
+    }
+
+
+    public String getTokenid() {
+        return tokenid;
+    }
+
 
     public String getUuid() {
         return uuid;
@@ -30,6 +90,8 @@ public class UserInfoDTO {
         this.uuid = uuid;
         return this;
     }
+
+
 
     public String getName() {
         return name;
@@ -84,4 +146,6 @@ public class UserInfoDTO {
         this.location = location;
         return this;
     }
+
+
 }
