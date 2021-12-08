958c39893b3f4662900d8aff75f8abedb6f1b930
zhugelijuan123 <nancyrain2015@163.com>
2021-12-08 17:24:32 +0800

    add userinfo and meviewmodel

--- EgamePlaygound/app/src/main/java/edu/neu/madcourse/numad21fa/egameplaygound/ui/me/MeViewModel.java
+++ EgamePlaygound/app/src/main/java/edu/neu/madcourse/numad21fa/egameplaygound/ui/me/MeViewModel.java
@@ -4,16 +4,35 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
+import edu.neu.madcourse.numad21fa.egameplaygound.constant.enums.user.UserLevelEnum;
+import edu.neu.madcourse.numad21fa.egameplaygound.model.dto.TeamUpCardDTO;
+import edu.neu.madcourse.numad21fa.egameplaygound.model.dto.UserInfoDTO;
+import edu.neu.madcourse.numad21fa.egameplaygound.ui.teamup.TeamUpCard;
+
 public class MeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private final MutableLiveData<UserInfoDTO> userInfoDTOLiveData;
+
 
     public MeViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is me fragment");
+        userInfoDTOLiveData = new MutableLiveData<>();
+
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+
+    public void updateUserLevel(UserInfoDTO userInfoDTO, UserLevelEnum value) {
+
+        userInfoDTO.setLevel(value);
+        userInfoDTOLiveData.setValue(userInfoDTO);
+    }
+
+
 }
